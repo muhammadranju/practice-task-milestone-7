367f33b85e68bc38ab4ea4529ed457490ac02d7d
Muhammad Ranju <mdranju23@gmail.com>
2024-10-10 11:36:43 +0600

    Refactor: Implement Tailwind CSS and DaisyUI

--- day_1/src/App.jsx
+++ day_1/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Counter from "./Counter";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex justify-center items-center flex-col h-screen space-y-5">
+        <h1 className="text-7xl font-bold">Vite + React</h1>
+        <Counter></Counter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ day_1/src/Counter.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [isVisible, setIsVisible] = useState(false); // Track visibility
+
+  const handelAdd = () => {
+    setCount(count + 1);
+    setIsVisible(true);
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 1000);
+  };
+
+  function handelMessage() {
+    handelAdd();
+  }
+  return (
+    <>
+      <h1 className="text-5xl font-bold">Counter</h1>
+      <div className="text-3xl font-semibold my-5">{count}</div>
+      <button className="btn" onClick={handelMessage}>
+        Add
+      </button>
+
+      <span
+        className={`text-2xl font-semibold my-5 b p-4 border-[2px] border-sky-500 rounded-xl  ${
+          isVisible ? "" : "hidden"
+        }`}
+      >
+        Added <span className="text-sky-500 font-bold text-3xl">{count}</span>{" "}
+        times
+      </span>
+    </>
+  );
+}
+
+export default Counter;
