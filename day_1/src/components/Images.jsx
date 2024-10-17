707e89a670123c11da2d4b12a1c06095953b9c42
Muhammad Ranju <mdranju23@gmail.com>
2024-10-17 07:55:33 +0600

    feat: update images file with looping image in array

--- day_1/src/components/Images.jsx
+++ day_1/src/components/Images.jsx
@@ -1,11 +1,6 @@
-import { signal } from "@preact/signals-react";
-
-export const setImages = signal([]);
-
 // eslint-disable-next-line no-unused-vars
-export default function Images({ img }) {
-  console.log();
-  return setImages.value.map((img, index) => (
+export default function Images({ petImages }) {
+  return petImages.map((img, index) => (
     <img
       key={index}
       src={img}
