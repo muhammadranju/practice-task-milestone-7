e11f8b2a05c623ecfee4c61411ecae017920c5b5
Muhammad Ranju <mdranju23@gmail.com>
2024-10-20 00:42:59 +0600

    feat: Add a placeholder for the countries
    - Created a CardPlaceholder component.
    - Implemented a loading state using `isLoading`.
    - Added conditional rendering for placeholder and country cards.
    - Displayed placeholders during data fetching.
    - Updated the country count heading.

---
+++ day_4/src/UI/CardPlaceholder.jsx
@@ -0,0 +1,47 @@
+const CardPlaceholder = () => {
+  return (
+    <div
+      role="status"
+      className="max-w-sm p-4 border rounded-xl shadow animate-pulse md:p-6 "
+    >
+      <div className="flex items-center justify-center w-full h-64  bg-gray-300 rounded-xl dark:bg-gray-400">
+        <svg
+          className="w-10 h-10 text-gray-200 dark:text-gray-300"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 16 20"
+        >
+          <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+          <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+        </svg>
+      </div>
+      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-48 mt-10 mb-4" />
+      {/* 
+      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5" />
+      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400 mb-2.5" />
+      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-400" /> */}
+      <div className="flex justify-between items-center mt-4">
+        <div>
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2" />
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2" />
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2" />
+          <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-400" />
+        </div>
+        <div>
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2" />
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2" />
+          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-400 w-32 mb-2" />
+          <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-400" />
+        </div>
+      </div>
+      <div className="flex justify-around items-center">
+        <div className="h-12 bg-gray-200 rounded-xl dark:bg-gray-400 w-28 mt-10 mb-4" />
+        <div className="h-12 bg-gray-200 rounded-xl dark:bg-gray-400 w-28 mt-10 mb-4" />
+      </div>
+      {/* <span className="sr-only">Loading...</span> */}
+    </div>
+  );
+};
+
+export default CardPlaceholder;
--- day_4/src/components/Countries.jsx
+++ day_4/src/components/Countries.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "./Country";
+import CardPlaceholder from "../UI/CardPlaceholder";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     async function getCountries() {
       const res = await fetch("https://restcountries.com/v3.1/all");
@@ -14,20 +15,38 @@ const Countries = () => {
         a.name.common.localeCompare(b.name.common)
       );
       setCountries(sortedCountries);
+      setIsLoading(false);
     }
     getCountries();
   }, []);
 
-  console.log(countries);
   return (
-    <div className="container mx-auto">
-      <h1>Countries: {countries.length}</h1>
-      <div className="grid grid-cols-4 gap-4">
-        {countries.map((country) => (
-          <Country key={country.cca3} country={country} />
-        ))}
+    <>
+      <div className="container mx-auto mt-5">
+        <h1 className="text-3xl font-bold my-5">
+          Countries: {countries.length > 0 ? countries.length : "Loading..."}
+        </h1>
+        <div className="grid grid-cols-4 gap-4">
+          {isLoading && (
+            <>
+              <CardPlaceholder />
+              <CardPlaceholder />
+              <CardPlaceholder />
+              <CardPlaceholder />
+              <CardPlaceholder />
+              <CardPlaceholder />
+              <CardPlaceholder />
+              <CardPlaceholder />
+            </>
+          )}
+
+          {!isLoading &&
+            countries.map((country) => (
+              <Country key={country.cca3} country={country} />
+            ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
